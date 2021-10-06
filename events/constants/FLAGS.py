173af23aa47de3fb549a606695907b4a84f86e80
Luke Lavan <luke.lavan01@gmail.com>
2021-10-05 21:53:31 -0400

    use docstrings for intellisense support

--- events/constants/FLAGS.py
+++ events/constants/FLAGS.py
@@ -1,43 +1,42 @@
 from soulstruct.game_types import Flag
 
+
 class FLAGS(Flag):
 
-    # enabled by default, disabled when Petrus teaches Shrug
     CanLearnShrug = 280
+    """ enabled by default, disabled when Petrus teaches Shrug """
 
-    # enabled by default, disabled when Shiva teaches Look SKyward
     CanLearnLookSkyward = 281
+    """ enabled by default, disabled when Shiva teaches Look SKyward """
 
-    # enabled by default, disabled when Andre teaches Hurrah!
     CanLearnHurrah = 282
+    """ enabled by default, disabled when Andre teaches Hurrah! """
 
-    # enabled by default, disabled when Oswald teaches Well! What is it?
     CanLearnWellWhatIsIt = 283
+    """ enabled by default, disabled when Oswald teaches Well! What is it? """
 
-    # enabled by default, disabled when Dusk teaches Proper Bow
     CanLearnProperBow = 284
+    """ enabled by default, disabled when Dusk teaches Proper Bow """
 
-    # enabled by default, disabled when Patches teaches Prostration
     CanLearnProstration = 285
+    """ enabled by default, disabled when Patches teaches Prostration """
 
-    # enabled by default, disabled when player learns Praise the Sun
     CanLearnPraiseTheSun = 286
+    """ enabled by default, disabled when player learns Praise the Sun """
 
-    # enabled once on first load, becomes disabled if the player class is knight or cleric
-    # only used to determine if Rhea can teach you Prayer gesture
-    # also disabled if Rhea teaches Prayer
-    # (these classes have this gesture available by default)
     CanLearnPrayer = 287
+    """ enabled once on first load, disabled if the player class is knight or cleric\n
+    becomes disabled once Rhea teaches Prayer\n
+    (these classes have this gesture available by default) """
 
-    # enabled once on first load, becomes disabled if player class isn't knight or cleric
-    # only used to determine if Domhnall can teach you Joy gesture
-    # also disabled if Domhnall teaches Joy
-    # (the other classes have this gesture available by default)
     CanLearnJoy = 288
+    """ enabled once on first load, disabled if player class isn't knight or cleric\n
+    becomes disabled once Domhnall teaches Joy\n
+    (the other classes have this gesture available by default) """
 
     # flags 289 and 290 are always on and both unused
     # they're enabled in the same event that handles the gesture flags above
 
-    # enabled after the two cutscenes play that warp the player
-    # from the asylum to Firelink (alongside awarding the achievement)
     HasWarpedToFirelinkShrine = 11810000
+    """ enabled after the two cutscenes play that warp the player
+    from the asylum to Firelink """
